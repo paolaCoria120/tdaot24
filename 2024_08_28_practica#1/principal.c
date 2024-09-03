29d80c7f486976e83f209f3654ef95c5453f55fa
paolaCoria120 <paolacoria120@gmail.com>
2024-09-02 20:08:27 -0600

    Update principal.c
    Se modifico el programa para evitar el desbordamiento de memoria para frases mayores a mil caracteres y para archivos mayores a 100 frases.

--- 2024_08_28_practica#1/principal.c
+++ 2024_08_28_practica#1/principal.c
@@ -13,38 +13,37 @@ int main(int argc, char* argv[])
     FILE* archivo = fopen(argv[1],"r");
     if(archivo == NULL)
     {
-        printf("No se puede abrir el arcvhivo %s\n", argv[1]); //determina si pudo leer el archivo
+        printf("No se puede abrir el arcvhivo %s\n", argv[1]); //determina si pudo leer y abrir el archivo
         return 1;
     }
 
     char caracter;
-    int n = 1;//por las especificaciones de la práctica, tiene al menos una frase
+    int n = 1; //por las especificaciones de la práctica, tiene al menos una frase
 
-    while ((caracter = fgetc(archivo)) != EOF)
-        if (caracter == '\n') // Contar las líneas cuando se encuentre un salto de línea
+    while ((caracter = fgetc(archivo)) != EOF && n <= 99) //evita contar mas de 100 frases
+    {
+        if (caracter == '\n') // sumar una linea cuando se encuentre un salto de línea
             n++;
-    rewind(archivo);
+    }
+    rewind(archivo); // rebobinar puntero de archivo
 
     char frases[n][1000];
     int resultados[n];
+    char trash;
 
     int c = 0, f = 0; //index de caracter y frase
-    while ((caracter = fgetc(archivo)) != EOF) {
+    while ((caracter = fgetc(archivo)) != EOF && f < n) //evita leer mas lineas que el numero de frases
+    {
         if (caracter == '\n') //identificar cambio de frase
-        {  
-            frases[f][c] = '\0';//insertar final de cadena al final de cada frase
+        {
+            frases[f][c] = '\0';//insertar final de cadena
             f++; //aumentar index de frase
             c = 0; //reiniciar index de caracter
-        } else {
+        } else if (c >= 999) trash = caracter;
+        else
+        {
             frases[f][c] = caracter; //guardar caracter
-            c++;
-            if (c >= 999)//prevenir el desbordamiento de memoria
-            {
-                printf("Advertencia: Se alcanzó la longitud máxima de la frase (1000 caracteres).\n");
-                frases[f][c] = '\0';
-                f++;
-                c = 0;
-            }
+            c++; //aumentar index de caracter
         }
     }
     frases[f][c]='\0';//fin de frase a la ultima frase
@@ -52,8 +51,18 @@ int main(int argc, char* argv[])
 
     son_pangramas(n + 1, frases, resultados); //evaluar frases
 
-    for (int i = 0; i < n; i++) //imprimir resultados
-        printf ("%d. %d\n", i+1, resultados[i]);
     
+    for (int i = 0; i < n; i++) //imprimir resultados
+    {
+        printf ("%d. ", i+1);
+        if (i == 0)
+            for (int j = 4; j < 14; j++)
+                printf ("%c", frases[i][j]);
+        else
+            for (int j = 0; j < 10; j++)
+                printf ("%c", frases[i][j]);
+        printf (": %d\n", resultados[i]);
+    }
+
     return 0;
 }
