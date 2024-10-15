00b93adf1ad59c9a1e5c68134820342a9e19ed2a
paolaCoria120 <paolacoria120@gmail.com>
2024-10-14 19:16:28 -0600

    Update principal.c
    Documentación.

--- tareas/2024_09_11_tarea_cumpleanios/principal.c
+++ tareas/2024_09_11_tarea_cumpleanios/principal.c
@@ -1,4 +1,17 @@
+/**
+ * @file principal.c
+ * @brief Función para gestionar una lista de cumpleaños.
+ * Este programa gestiona una lista de cumpleaños, permitiendo al usuario crear una lista, 
+ * insertar nodos, buscar y eliminar cumpleaños.
+ */
+
 #include "header.h"
+/**
+ * @brief Función principal del programa.
+ * Esta función inicializa el programa, limpia la pantalla y llama a la función de menú 
+ * para que el usuario interactúe con la lista de cumpleaños.
+ * @return 0 si se ejecuta correctamente.
+ */
 
 int main(void){
         
